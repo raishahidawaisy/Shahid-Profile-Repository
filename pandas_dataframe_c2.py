9ae1a6bb904826baf84c914b860261c1567c8d44
Shahid Abbas <raishahidawaisy@gmail.com>
2020-02-06 11:50:26 +0500

    Pandas Data Frame-2, to read from CSV into DataFrame

--- pandas_dataframe_c2.py
+++ pandas_dataframe_c2.py
@@ -1,14 +1,22 @@
-#Task1: To change list/dictionary to dataframe and remove columns and rows
+#Task1: To read csv file using pandas and extracting data in different ways
 
 # import pandas as pd
 import pandas as pd
 
-# making data frame from csv file 
-data = pd.read_csv("nba.csv", index_col ="Name") 
+# Case1:making data frame from csv file and setting 3rd column as index column
+data = pd.read_csv("pandas_dataframe_c2.csv", index_col ="Run Status") 
   
-# retrieving row by loc method 
-first = data.loc["Avery Bradley"] 
-second = data.loc["R.J. Hunter"] 
+# retrieving column by loc method 
+run_status_pass = data.loc["Pass"]
+run_status_fail = data.loc["Fail"]   
+print(run_status_pass)
+print(run_status_fail)
+
+# Case2:making data frame from csv file and setting first column as Index column 
+data = pd.read_csv("pandas_dataframe_c2.csv", index_col ="Test Name") 
   
+# retrieving column by loc method 
+test_name1 = data.loc[["Test 1","Test 3"]] 
+test_name2 = data.loc["Test 10"] 
   
-print(first, "\n\n\n", second) 
+print(test_name1, "\n\n\n", test_name2) 
